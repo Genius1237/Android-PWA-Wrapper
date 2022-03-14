4b60fa847fea2c4c193d95148fde4964c8d8f9b0
Genius1237 <anirudhsriniv@gmail.com>
2022-03-14 15:17:29 -0500

    Update Constants.java

--- app/src/main/java/at/xtools/pwawrapper/Constants.java
+++ app/src/main/java/at/xtools/pwawrapper/Constants.java
@@ -7,10 +7,10 @@ public class Constants {
     public static String WEBAPP_HOST = "whatsapp.com"; // used for checking Intent-URLs
 
 	// User Agent tweaks
-    public static boolean POSTFIX_USER_AGENT = true; // set to true to append USER_AGENT_POSTFIX to user agent
-    public static boolean OVERRIDE_USER_AGENT = false; // set to true to use USER_AGENT instead of default one
+    public static boolean POSTFIX_USER_AGENT = false; // set to true to append USER_AGENT_POSTFIX to user agent
+    public static boolean OVERRIDE_USER_AGENT = true; // set to true to use USER_AGENT instead of default one
     public static String USER_AGENT_POSTFIX = "AndroidApp"; // useful for identifying traffic, e.g. in Google Analytics
-    public static String USER_AGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36";
+    public static String USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0";
 	
 	// Constants
     // window transition duration in ms
